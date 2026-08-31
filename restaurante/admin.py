b4148c5f0413955471e1f2c0bb23b57c638ecf25
davidn05 <nogueradavid005@gmail.com>
2026-08-31 15:30:32 -0300

    chore: registrar entidades en el panel de administracion

--- restaurante/admin.py
+++ restaurante/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Rol,
+    Empleado,
+    Mesa,
+    Categoria,
+    Producto,
+    ProductoSugerido,
+    PedidoCabecera,
+    PedidoDetalle,
+    Reserva,
+    MetodoPago,
+    Factura,
+    Pago,
+    CotizacionMoneda,
+    Caja,
+    MovimientoCaja,
+)
+
+admin.site.register(Rol)
+admin.site.register(Empleado)
+admin.site.register(Mesa)
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(ProductoSugerido)
+admin.site.register(PedidoCabecera)
+admin.site.register(PedidoDetalle)
+admin.site.register(Reserva)
+admin.site.register(MetodoPago)
+admin.site.register(Factura)
+admin.site.register(Pago)
+admin.site.register(CotizacionMoneda)
+admin.site.register(Caja)
+admin.site.register(MovimientoCaja)
