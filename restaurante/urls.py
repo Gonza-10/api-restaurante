b5fbcd2ecb1350875b76c6d8ddb175d95cddc99e
davidn05 <nogueradavid005@gmail.com>
2026-08-31 15:30:18 -0300

    feat: exponer endpoints REST para las nuevas entidades

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductoViewSet, MesaViewSet, MozoViewSet
+
+from .views import (
+    CategoriaViewSet,
+    ProductoViewSet,
+    MesaViewSet,
+    EmpleadoViewSet,
+    PedidoCabeceraViewSet,
+    PedidoDetalleViewSet,
+)
 
 router = DefaultRouter()
-router.register(r'productos', ProductoViewSet)
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'productos', ProductoViewSet, basename='producto')
 router.register(r'mesas', MesaViewSet)
-router.register(r'mozos', MozoViewSet)
+router.register(r'empleados', EmpleadoViewSet)
+router.register(r'pedidos', PedidoCabeceraViewSet)
+router.register(r'detalles-pedido', PedidoDetalleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- restaurante/views.py
+++ restaurante/views.py
@@ -1,15 +1,54 @@
 from rest_framework import viewsets
-from .models import Producto, Mesa, Mozo
-from .serializers import ProductoSerializer, MesaSerializer, MozoSerializer
+
+from .models import Categoria, Producto, Mesa, Empleado, PedidoCabecera, PedidoDetalle
+from .serializers import (
+    CategoriaSerializer,
+    ProductoSerializer,
+    MesaSerializer,
+    EmpleadoSerializer,
+    PedidoCabeceraSerializer,
+    PedidoDetalleSerializer,
+)
+
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
 
 class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+    def get_queryset(self):
+        queryset = Producto.objects.select_related('categoria').all()
+        if self.action == 'list':
+            categoria_id = self.request.query_params.get('categoria')
+            vegetariano = self.request.query_params.get('vegetariano')
+            celiaco = self.request.query_params.get('celiaco')
+            if categoria_id:
+                queryset = queryset.filter(categoria_id=categoria_id)
+            if vegetariano == 'true':
+                queryset = queryset.filter(es_vegetariano=True)
+            if celiaco == 'true':
+                queryset = queryset.filter(apto_celiaco=True)
+        return queryset
+
 
 class MesaViewSet(viewsets.ModelViewSet):
     queryset = Mesa.objects.all()
     serializer_class = MesaSerializer
 
-class MozoViewSet(viewsets.ModelViewSet):
-    queryset = Mozo.objects.all()
-    serializer_class = MozoSerializer
+
+class EmpleadoViewSet(viewsets.ModelViewSet):
+    queryset = Empleado.objects.select_related('rol').all()
+    serializer_class = EmpleadoSerializer
+
+
+class PedidoCabeceraViewSet(viewsets.ModelViewSet):
+    queryset = PedidoCabecera.objects.select_related('mesa', 'empleado').prefetch_related('detalles__producto').all()
+    serializer_class = PedidoCabeceraSerializer
+
+
+class PedidoDetalleViewSet(viewsets.ModelViewSet):
+    queryset = PedidoDetalle.objects.select_related('producto', 'pedido_cabecera').all()
+    serializer_class = PedidoDetalleSerializer
